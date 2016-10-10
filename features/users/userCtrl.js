13d312c439c901897e5c953287fd2ddfc50d151b
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-10 13:36:16 -0500

    Simplified getAuthUser and userExists endpoints/functions

--- features/users/userCtrl.js
+++ features/users/userCtrl.js
@@ -2,44 +2,16 @@ import User from "./User";
 
 export default {
 	getAuthUser(req, res) {
-		User.findOne( {}, (err, user) => {
-			if (user) {
-				User.findById(user._id)
-					.populate()
-					.populate()
-					.populate()
-					.exec((error, currentUser) => {
-						if (error) {
-							return res.status(500).json(error);
-						}
-						return res.status(200).json(currentUser);
-					});
-			} else if (err) {
-				return res.status(500).json(err);
-			}
-		});
+    res.send(req.user)
 	},
 
 	userExists(req, res, next) {
-		if (!req.user) throw new Error('user null');
-		Users.findOne( {email: req.user._json.email}, (err, user) => {
-			if (err) return res.redirect('/#/error');
-			if (user) return res.redirect('/#/user');
+		if (!req.user) throw new Error("Unknown User");
+		User.findOne({"facebook_id": profile.id}, (err, user) => {
+			if (err) return res.redirect("/#/error");
+			if (user) return res.redirect("/#/user");
 			next();
-		} );
+		});
 	},
 
-	createUser(req, res) {
-    new User({
-      displayName: req.user._json.displayName,
-      email: req.user._json.email,
-      creationDate: new Date(),
-      photo: `https://graph.facebook.com/${req.user.identities[0].user_id}/picture?width=9999`
-    }).save((err, newUser) => {
-      if (err) {
-        return res.redirect('/#/error');
-      }
-      return res.redirect('/#/user');
-    });
-  }
 };
--- features/users/userRoutes.js
+++ features/users/userRoutes.js
@@ -3,13 +3,14 @@ import passport from "passport";
 import auth from "../../middleware/auth";
 
 export default function(app) {
-  app.get("/callback",
+  app.get("/auth/facebook", passport.authenticate("facebook");
+  app.get("/auth/facebook/callback",
     passport.authenticate("facebook", {failureRedirect: "/"}),
-    userCtrl.userExists,
-    userCtrl.createUser
+    userCtrl.userExists
+    // , userCtrl.createUser
   );
 
- app.get('/user', auth.isLoggedIn, userCtrl.getProfile);
+ app.get('/user', auth.isLoggedIn, userCtrl.getAuthUser);
 
  app.get('/logout', function(req, res) {
       req.logout();
