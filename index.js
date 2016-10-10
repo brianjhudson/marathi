b9d0fa3b4bcde03a2260eba8611dfad8d485bc79
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-10 14:12:42 -0500

    Downgrading from import to require syntax in server files

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express from "express";
-import sessionConfig from "./config/config";
-const app = express()
+const express = require("express")
+    , sessionConfig = require("./config/config")
+    , app = express()
     , port = process.env.PORT || 8800;
 
 // JSON and Session
-import {json} from "body-parser";
-import session from "express-session";
+const {json} = require("body-parser")
+    , session = require("express-session");
 app.use(json());
 app.use(express.static(`${__dirname}/public`));
 app.use(session({secret: sessionConfig.secret}));
 
 // Passport
-import passport from "passport";
-import strategy from "./config/strategy";
+const passport = require("passport")
+    , strategy = require("./config/strategy");
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(strategy);
@@ -20,13 +20,12 @@ passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
 // Mongoose
-import mongoose from "mongoose";
-import dbConfig from "./config/database.js";
+const mongoose = require("mongoose")
+    , dbConfig = require("./config/database.js");
 mongoose.connect(dbConfig.mongoUri);
 mongoose.connection.once("open", () => console.log(`Connected to MongoDB at ${dbConfig.mongoUri}`));
 
 //Routes
-import masterRoutes from "./masterRoutes";
-masterRoutes(app);
+require("./masterRoutes")(app);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
