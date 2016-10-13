aea0e1425be5954bfd74a743e128a919e5837ad4
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-13 14:56:15 -0500

    "Updated getUser to send json only"

--- server/features/users/User.js
+++ server/features/users/User.js
@@ -4,6 +4,8 @@ const User = new mongoose.Schema({
   facebook_id: {type: String, required: true, unique: true}
   , name: {type: String, required: true}
   , email: {type: String, unique: true}
+  , photo: { type: String, default: 'http://www.clker.com/cliparts/m/3/I/C/c/2/grey-silhouette-of-man.svg' }
+  // , lessons: [{type: string}]
 });
 
 module.exports = mongoose.model("User", User);
