92c9e51b341c1680abb76586d4c7bf9860b0b1e2
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-13 17:16:01 -0500

    "Updated Term model to include review questions"

--- server/features/lessons/Term.js
+++ server/features/lessons/Term.js
@@ -3,13 +3,19 @@ const mongoose = require('mongoose')
       term: {type: String, trim: true, required: true}
     , transliteration: {type: String, trim: true}
     , explanation: {type: String}
-    , category: {type: String, enum: ["grammar", "alphabet", "vocabulary"]}
+    , category: {type: String, enum: ["grammar", "alphabet", "vocabulary", "culture"]}
     , theme: {type: String}
+    , image: {type: String}
+    , sound: {type: String}
     , example: {
         use: {type: String}
         , transliteration: {type: String}
         , translation: {type: String}
       }
+    , reviewQuestion: {
+        question: {type: String}
+        , answer: {type: String}
+    }
     })
 
 module.exports = mongoose.model("Term", Term)
