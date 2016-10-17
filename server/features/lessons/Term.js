e1f845ad75d9e2a0a8f90d9df2c1db332a5960ac
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-17 12:53:29 -0500

    "Added author and review properties to Term schema"

--- server/features/lessons/Term.js
+++ server/features/lessons/Term.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose')
   , Term = new mongoose.Schema({
-      term: {type: String, trim: true, required: true}
+      author: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
+    , authorImage: {type: String}
+    , language: {type: String, trim: true, required: true}
+    , term: {type: String, trim: true, required: true}
     , transliteration: {type: String, trim: true}
-    , explanation: {type: String}
-    , category: {type: String, enum: ["grammar", "alphabet", "vocabulary", "culture"]}
+    , explanation: {type: String, required: true}
+    , category: {type: String, enum: ["grammar", "alphabet", "vocabulary", "culture"], required: true}
+    , topic: {type: String}
     , theme: {type: String}
+    , lesson: {type: mongoose.Schema.Types.ObjectId, ref: "Lesson"}
     , image: {type: String}
+    , imageCredit: {type: String}
     , sound: {type: String}
-    , example: {
-        use: {type: String}
-        , transliteration: {type: String}
-        , translation: {type: String}
-      }
-    , reviewQuestion: {
-        question: {type: String}
-        , answer: {type: String}
-    }
+    , example: {type: String}
+    , exampleTransliteration: {type: String}
+    , exampleTranslation: {type: String}
+    , reviewQuestion: {type: String}
+    , reviewAnswer: {type: String}
+    , reviewAnswerType: {type: String, enum: ['term', 'transliteration', 'definition', 'translation']}
+    , reviewImage: {type: String}
+
     })
 
 module.exports = mongoose.model("Term", Term)
