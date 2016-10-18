ea2b57562f329c86d8a0a8117eaf2a71fe21164f
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-18 17:25:40 -0500

    "Moved postTerm function from userController to termEntry directive"

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,15 +11,6 @@ function userController($scope, userService, lessonService) {
     })
   }
 
-  $scope.postTerm = term => {
-    term.author = $scope.currentUser._id;
-    term.language = "Marathi";
-    lessonService.postTerm(term).then(result => {
-      $scope.newTerm = {};
-      return result;
-    })
-  }
-
   init();
 
 }
--- src/directives/termEntryModal/termEntryModal.js
+++ src/directives/termEntryModal/termEntryModal.js
@@ -5,11 +5,19 @@ function termEntryModal() {
     restrict: "E"
     , replace: true
     , template: termEntryModalTemp
-    , scope: {
-        postTerm: "&"
-    }
+    , scope: {}
     , controller: function($state, $scope) {
         $scope.stateName = $state.current.name;
+        $scope.newTerm = {};
+        $scope.postTerm = term => {
+          term.author = $scope.currentUser._id;
+          term.language = "Marathi";
+          lessonService.postTerm(term).then(result => {
+            $scope.newTerm = {};
+            return result;
+          })
+        }
+
     }
   }
 }
