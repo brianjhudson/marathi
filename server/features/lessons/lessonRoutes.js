fe27aebd1215b0cafd0be2f94137bd43cdc476a1
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-18 15:03:16 -0500

    "Added getLesson functions to lessonRoutes"

--- server/features/lessons/lessonRoutes.js
+++ server/features/lessons/lessonRoutes.js
@@ -1,6 +1,8 @@
 const lessonCtrl = require("./lessonCtrl");
 
 module.exports = app => {
-  app.post('/api/lessons', lessonCtrl.postLesson);
-  app.post('/api/terms', lessonCtrl.postTerm);
+  app.post("/api/lessons", lessonCtrl.postLesson);
+  app.post("/api/terms", lessonCtrl.postTerm);
+  app.get("/api/lessons/:id", lessonCtrl.getLessonById);
+  app.get("/api/lessons", lessonCtrl.getLessons);
 }
