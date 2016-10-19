aefea5aceda80d35e8353a9d4f9fcd9825217a17
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-19 08:34:35 -0500

    "Added getLessonById to retrieve lesson details"

--- src/directives/lessonListDisplay/lessonListDisplay.js
+++ src/directives/lessonListDisplay/lessonListDisplay.js
@@ -11,11 +11,21 @@ function lessonListDisplay() {
     , link: function(scope, element, attr) {
 
     }
-    , controller: function($scope, $rootScope, userService) {
+    , controller: function($scope, $rootScope, userService, lessonService) {
         $scope.currentUser = userService.currentUser;
         $rootScope.$on("userUpdate", function(event, data) {
           $scope.currentUser = data;
-          console.log($scope.currentUser);
+          for (let i = 0; i < $scope.currentUser.lessons.length; i++) {
+            $scope.currentUser.lessons[i].loading = true;
+            lessonService.getLessonById($scope.currentUser.lessons).then(result => {
+              console.log(result);
+              // TODO: Check result to see format of lesson;
+              $scope.currentUser.lessons[i].lessonDetails = result.data;
+              $scope.currentUser.lessons[i].loading = false;
+            })
+
+          }
+          // TODO: Add regular getLessons for guest
         });
 
     }
