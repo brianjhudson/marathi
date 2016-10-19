7dccd987c5e770a76d0face81d39c1cf37113a3c
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-19 10:25:17 -0500

    "Initialized selectedLesson to first incomplete lesson"

--- src/directives/lessonListDisplay/lessonListDisplay.js
+++ src/directives/lessonListDisplay/lessonListDisplay.js
@@ -16,17 +16,22 @@ function lessonListDisplay() {
         $rootScope.$on("userUpdate", function(event, data) {
           $scope.currentUser = data;
           for (let i = 0; i < $scope.currentUser.lessons.length; i++) {
+            // Show preloader
             $scope.currentUser.lessons[i].loading = true;
+            // Set selectedLesson to first incomplete lesson // Maybe move to lessonService
+            if (!$scope.selectedLesson._id && $scope.currentUser.lessons[i].completed === false) {
+              $scope.selectedLesson = $scope.currentUser.lessons[i];
+            }
             lessonService.getLessonById($scope.currentUser.lessons[i]._id).then(result => {
               console.log(result);
               // TODO: Check result to see format of lesson;
               $scope.currentUser.lessons[i].lessonDetails = result.data;
               $scope.currentUser.lessons[i].loading = false;
             })
-
           }
           // TODO: Add regular getLessons for guest
         });
+
 
     }
 
