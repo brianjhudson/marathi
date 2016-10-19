4914da2a45765085e3d9ad14b56f870705f21d06
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-19 12:14:43 -0500

    "Removed selectedLesson (now on user document)"

--- src/directives/lessonDisplay/lessonDisplay.js
+++ src/directives/lessonDisplay/lessonDisplay.js
@@ -5,24 +5,16 @@ function lessonDisplay() {
     restrict: "E"
     , replace: true
     , template: lessonDisplayTemp
-    , scope: {
-        selectedLesson: "="
-    }
+    , scope: {}
     , link: function(scope, element, attr) {
 
     }
     , controller: function($scope, $rootScope, userService, lessonService) {
         $scope.currentUser = userService.currentUser;
-        $rootScope.$on("userUpdate", function(event, data) {
-          //TODO: Move to userService
-          $scope.currentUser = data;
+        $rootScope.$on("userUpdate", function(event, user) {
+          $scope.currentUser = user;
         });
-        $scope.selectedLesson.loading = true;
-        lessonService.getSelectedLesson($scope.selectedLesson._id).then(result => {
-          console.log(result);
-          $scope.selectedLesson = result;
-          $scope.selectedLesson.loading = false;
-        })
+
 
     }
 
