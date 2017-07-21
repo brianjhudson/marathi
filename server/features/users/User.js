fcc3d54a9a2c711301689645c15e1ef0f7599334
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-21 10:21:09 -0500

    Removed required prop from facebook_id

--- server/features/users/User.js
+++ server/features/users/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
     , Lesson = require("../lessons/Lesson");
 
 const User = new mongoose.Schema({
-  facebook_id: {type: String, required: true, unique: true}
+  facebook_id: {type: String}
   , name: {type: String, required: true}
-  , email: {type: String, unique: true}
+  , email: {type: String}
   , photo: {type: String, default: 'http://www.clker.com/cliparts/m/3/I/C/c/2/grey-silhouette-of-man.svg'}
   , dateJoined: {type: Date, default: new Date()}
   , lastLogin: {type: Date, default: new Date()}
